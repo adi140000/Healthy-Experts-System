b250f87a7613a9c24538d8348a894c5bf440f54e
unknown <adi120.a@gmail.com>
2019-06-11 22:15:27 +0200

    Create server.js

--- src/part/Main.js
+++ src/part/Main.js
@@ -1,71 +1,123 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
-    state = {}
+    state = {
+        age: '',
+        sex: 'male',
+        temperature: '',
+        pressure: '',
+        pain: [],
+        nodes: '',
+        tired: true,
+        overweight: false,
+        breath: false,
+        vomiting: false
+    }
 
     submitData = (e) => {
         e.preventDefault();
         console.log(e.target)
     }
 
+    handle = (e) => {
+        const { target } = e;
+        const { id, value } = target;
+
+        if (target.hasOwnProperty('value') || id === 'sex') {
+            console.log(target)
+            this.setState({
+                [id]: value,
+            })
+        } else if (target.hasOwnProperty('checked')) {
+
+            if (target.name === 'pain') {
+                let { pain } = this.state;
+
+                if (target.checked) {
+                    pain.push(value);
+                    this.setState({
+                        pain
+                    })
+                } else {
+                    const newArr = pain.filter(item => {
+                        if (item === value) {
+                            return false
+                        } else {
+                            return true
+                        }
+                    })
+                    console.log(newArr)
+                    this.setState({
+                        pain: newArr,
+                    })
+                }
+            } else {
+                this.setState({
+                    [id]: target.checked,
+                })
+            }
+        }
+    }
+
     render() {
+        const { age, sex, temperature, pressure, pain, nodes, tired, overweight, breath, vomiting } = this.state;
         return (
-            <main>
+            < main >
                 <form className='form-main' onSubmit={this.submitData}>
                     <div className='form-part'>
                         <label htmlFor='age' className='label'>Ile masz lat ?</label>
-                        <input id='age' className='inputData part' type='number' />
+                        <input id='age' className='inputData part' type='number' value={age} onChange={this.handle} />
                     </div>
                     <div className='form-part'>
                         <label className='label'>Jakiej jestes płci ?</label>
-                        <select id='sex' className='inputData part'>
-                            <option>Meżczyzna</option>
-                            <option>Kobieta</option>
+                        <select id='sex' className='inputData part' value={sex} onChange={this.handle}>
+                            <option value='male'>Mezczyzna</option>
+                            <option value='female'>Kobieta</option>
                         </select>
                     </div>
                     <div className='form-part'>
                         <label htmlFor='temperature' className='label'>Jaka jest temperatura twojego ciała ?</label>
-                        <input type='number' id='temperature' className='inputData part' />
+                        <input onChange={this.handle} value={temperature} type='number' id='temperature' className='inputData part' />
                     </div>
 
                     <div className='form-part'>
                         <label className='label' htmlFor='pressure'>Jakie masz ciśnienie krwi ?</label>
-                        <input type='number' id='pressure' className='inputData part' />
+                        <input onChange={this.handle} value={pressure} type='number' id='pressure' className='inputData part' />
                     </div>
                     <div className='form-part'>
-                        <label className='label' >Odczuwasz jakisz ból ?</label>
-                        <div className='part clearfix'>
-                            <label><input type='checkbox' name='pain' value='Brzuch' />Brzuch</label>
-                            <label><input type='checkbox' name='pain' value='Brzuch' />Glowa</label>
-                            <label><input type='checkbox' name='pain' value='Brzuch' />Serce</label>
-                            <label><input type='checkbox' name='pain' value='Brzuch' />Kregoslup</label>
+                        <label className='label' >Odczuwasz jakis ból ?</label>
+                        <div id='pain' className='part clearfix'>
+                            <label><input onChange={this.handle} type='checkbox' name='pain' value='Stomach' />Brzuch</label>
+                            <label><input onChange={this.handle} type='checkbox' name='pain' value='Head' />Glowa</label>
+                            <label><input onChange={this.handle} type='checkbox' name='pain' value='Heart' />Serce</label>
+                            <label><input onChange={this.handle} type='checkbox' name='pain' value='Spine' />Kregoslup</label>
                         </div>
                     </div>
 
                     <div className='form-part'>
-                        <label className='label' htmlFor='bind'>Czy masz powiekszone węzły chłonne ?</label>
-                        <input type='checkbox' id='bind' className='inputData part' />
+                        <label className='label' htmlFor='nodes'>Czy masz powiekszone węzły chłonne ?</label>
+                        <input onChange={this.handle} checked={nodes} type='checkbox' id='nodes' className='inputData part' />
                     </div>
                     <div className='form-part'>
                         <label className='label' htmlFor='tired'>Czy szybko się męczysz ?</label>
-                        <input type='checkbox' className='inputData part' id='tired' />
+                        <input onChange={this.handle} checked={tired} type='checkbox' className='inputData part' id='tired' />
                     </div>
                     <div className='form-part'>
                         <label className='label' htmlFor='overweight'>Czy masz nadwage ?</label>
-                        <input type='checkbox' className='inputData part' id='overweight' />
+                        <input onChange={this.handle} checked={overweight} type='checkbox' className='inputData part' id='overweight' />
                     </div>
                     <div className='form-part'>
                         <label className='label' htmlFor='breath'>Czy masz problemy z drogami oddechowymiswwww swsw wssw ?</label>
-                        <input type='checkbox' className='inputData part' id='breath' />
+                        <input onChange={this.handle} checked={breath} type='checkbox' className='inputData part' id='breath' />
                     </div>
                     <div className='form-part'>
-                        <label className='label' htmlFor='overweight'>Czy miałeś w ostatnim czasie wymioty / nudnosci / biegunke ?</label>
-                        <input type='checkbox' className='inputData part' id='overweight' />
+                        <label className='label' htmlFor='vomiting'>Czy miałeś w ostatnim czasie wymioty / nudnosci / biegunke ?</label>
+                        <input onChange={this.handle} checked={vomiting} type='checkbox' className='inputData part' id='vomiting' />
                     </div>
 
                     <input type='submit' className='btnSend' value='Sprawdz' />
                 </form>
-            </main>
+            </main >
         );
     }
 }
